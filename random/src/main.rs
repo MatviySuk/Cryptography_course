d64046b0790d23af44cdced9d02a036c0159eba0
MatviySuk <enwilco@gmail.com>
2023-09-09 11:43:01 +0300

    Develop LCG random and cli tool to generate values

---
+++ random/src/main.rs
@@ -0,0 +1,50 @@
+use clap::Parser;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Multiplier value
+    #[arg(short)]
+    a: i32,    
+
+    /// Seed or start value
+    #[arg(short, long)]
+    seed: i32,
+
+    /// Modulus value
+    #[arg(short, long)]
+    modulus: i32, 
+
+    /// Increment value
+    #[arg(short)]
+    c: i32,
+
+    /// File name
+    #[arg(short, long, default_value_t = 10)]
+    iteration: i32,     
+
+    /// File name
+    #[arg(short, long, default_value = "random.txt")]
+    file_name: String,       
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut lcg_random = random::LCGRandom::new(args.a, args.c, args.modulus, args.seed);
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(args.file_name)
+        .expect("Failed to open file.");
+
+    for _ in 0..args.iteration {
+        let seed = lcg_random.generate();
+
+        println!("{}", seed);
+        file.write_all(format!("{}\n", seed).as_bytes()).expect("Failed to write into file.");
+    }
+}
