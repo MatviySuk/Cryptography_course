fa1adaa9e60449112eefabdae7f73e7bef3e8f0e
MatviySuk <enwilco@gmail.com>
2023-09-26 14:43:54 +0300

    Update RC5 to support 16, 32, 64 bits words lenght

--- rc5_algo/src/main.rs
+++ rc5_algo/src/main.rs
@@ -1,6 +1,4 @@
-use random::LCGRandom;
-use std::time::SystemTime;
-
+use rc5_algo::RC5WordSize;
 use clap::Parser;
 use std::fs;
 
@@ -25,46 +23,40 @@ struct Args {
     save_path: String,
 
     /// Number of rounds
+    #[arg(short, long, default_value_t = 32)]
+    word_size: u8,
+
+    /// Number of rounds
     #[arg(short, long, default_value_t = 16)]
     rounds: u8,
 
     /// Number of octets in key
-     #[arg(short, long, default_value_t = 8)]
-    bytes_key: u8
+    #[arg(short, long, default_value_t = 8)]
+    bytes_key: u8,
 }
 
 fn main() {
     let args = Args::parse();
-    
-    let rc5_32 = rc5_algo::RC5_32::new(args.rounds, args.bytes_key);
-    let key_hash = md5_algo::compute(args.key.as_bytes().to_vec());
 
-    let key = match args.bytes_key {
-        8 => key_hash.0[8..].to_vec(),
-        16 => key_hash.0.to_vec(),
-        32 => [md5_algo::compute(key_hash.0.to_vec()).0, key_hash.0].concat(),
-        _ => unreachable!("Incorrect octets number in key"),   
+    let word_size = match args.word_size {
+        16 => RC5WordSize::Bits16,
+        32 => RC5WordSize::Bits32,
+        64 => RC5WordSize::Bits64,
+        _ => unreachable!("Wrong word size provided: Accept only: 16, 32, 64."),
     };
 
+    let rc5 = rc5_algo::RC5::new(word_size, args.rounds, args.bytes_key);
+    let key = rc5.generate_key(args.key.as_bytes());
     let data = fs::read(args.file_path).expect("Unable to read data from file");
 
     match args.operation {
         0 => {
-            let seed = SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .expect("Failed to generate seed")
-                .as_nanos();
-            let mut lcg_rand = LCGRandom::new(1103515245, 12345, 2147483647, seed as u32);
-
-            let iv: [u8; 8] =
-                ((lcg_rand.generate() as u64) | ((lcg_rand.generate() as u64) << 32)).to_le_bytes();
-
-            let cypher = rc5_32.encrypt_cbc_pad(&iv, &data, &key);
+            let cypher = rc5.encrypt_cbc_pad(&data, &key);
 
             fs::write(args.save_path, cypher.0).expect("Failed to cypher text to file");
         }
         1 => {
-            let decrypted = rc5_32.decrypt_cbc_pad(&data, &key);
+            let decrypted = rc5.decrypt_cbc_pad(&data, &key);
 
             fs::write(args.save_path, decrypted.0)
                 .expect("Failed to save decryption result to file");
